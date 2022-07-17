f40b834aeeb3b061d2bb32d7731ad8aaadd21dff
AverageRobloxEnjoyer <109462410+AverageRobloxEnjoyer@users.noreply.github.com>
2022-07-17 06:44:51 -0400

    wtf
    what the fuckity fuck fuck is dat!

---
+++ roblox fucking simulator.lua
@@ -0,0 +1,68 @@
+local DiscordLib =
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+
+local win = DiscordLib:Window("TITANIUM HUB VERSION 1.0.0 UNRELEASED PREVIEW")
+
+local serv = win:Server("Titanium Hub", "poop")
+local normalscripts = serv:Channel("Scripts")
+local lol = serv:Channel("Sex Scripts")
+local btns = serv:Channel("Settings")
+
+btns:Button(
+    "Coming soon!",
+    function()
+        
+
+    end
+)
+
+lol:Button(
+    "Sex",
+    function()
+        
+
+    end
+)
+
+lol:Button(
+    "Sex V2 - NOT DONE",
+    function()
+        
+
+    end
+)
+
+
+lol:Button(
+    "R64 Stand - NOT DONE",
+    function()
+        
+
+    end
+)
+
+
+lol:Button(
+    "Furry Maid - NOT DONE",
+    function()
+        
+
+    end
+)
+
+
+
+lol:Button(
+    "Give friend sex Scripts - NOT DONE",
+    function()
+        
+    end
+)
+
+normalscripts:Button(
+    "Coming soon!",
+    function()
+        
+
+    end
+)
